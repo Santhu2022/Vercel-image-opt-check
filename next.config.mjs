f9af354a43324f05422e0892a3ed4f908492cc55
Santhu2022 <mogilipakasantosh@gmail.com>
2024-03-13 12:28:17 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "weddingimage.betterhalf.ai"
+    ],
+  }
 };
 
 export default nextConfig;
